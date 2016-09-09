3cab9d929284ab024019dacf73823f322b5f3627
Andrew Daly <andrew.daly@students.ittralee.ie>
2016-09-09 18:13:07 +0100

    Added a print results function that loads the JSON data from the url into a python object. The object can then be manipulated.

--- quake_data.py
+++ quake_data.py
@@ -1,16 +1,27 @@
 from urllib.request import urlopen
+import json
 
 
 # Retrieve earthquake data for the past hour relating to earthquakes of magnitude 2.5+.
+
+def print_results(data):
+    # Load(deserialize a str instance) quake event data into python object
+    data_json = json.loads(data.decode())
+    # access the contents of the JSON
+    if "title" in data_json["metadata"]:
+        print(data_json["metadata"]["title"])
 
 def main():
     url = urlopen("http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_hour.geojson")
     print("Result code: " + str(url.getcode()))
     if url.getcode() == 200:
         data = url.read()
-        print(data)
+        print_results(data)
     else:
         print("Error, cannot retrieve results " + str(url.getcode()))
+
+
+
 
 
 if __name__ == '__main__':
